f12f84c48f9e4903772a351f944d490138cf93fb
Bryan Goldstein <goldstein.bryan@bcg.com>
2017-10-18 23:05:42 -0400

    fix breaking changes in deep-merge

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const merge = require("deepmerge");
 const graphqlMask = require("graphql-mask");
 
+const defaultMergeOptions = {
+  arrayMerge: function(destinationArray, sourceArray, options) {
+    const byId = {};
+    destinationArray.forEach(function(item, i) {
+      const index = (typeof item === "object") ? item.id : null || i;
+      byId[index] = item
+    });
+    sourceArray.forEach(function(item, i) {
+      const index = (typeof item === "object") ? item.id : null || destinationArray.length+i
+      byId[index] = Object.assign(byId[index] || {}, item);
+    });
+    return Object.values(byId);
+  }
+}
+
 module.exports = function graphqlGateway(metaSchema, query, mergeOptions) {
   return merge.all(
     metaSchema.map(function(schemaInfo) {
       return schemaInfo.resolve(graphqlMask(schemaInfo.schema, query));
     }),
-    mergeOptions
+    mergeOptions || defaultMergeOptions
   );
 };
