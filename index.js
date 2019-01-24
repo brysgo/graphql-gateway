5fc392fe2f7f42b61e055ba12d8b4016a31d18aa
Bryan Goldstein <brysgo@gmail.com>
2019-01-23 19:41:19 -0500

    switch from deepmerge to merge-resolver

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-const merge = require("deepmerge");
+const flat = require("array.prototype.flat");
+const { mergeResolver } = require("merge-resolver");
 const graphqlMask = require("graphql-mask");
 
-const defaultMergeOptions = {
-  arrayMerge: function(destinationArray, sourceArray, options) {
-    const byId = {};
-    destinationArray.forEach(function(item, i) {
-      const index = (typeof item === "object") ? item.id : null || i;
-      byId[index] = item
-    });
-    sourceArray.forEach(function(item, i) {
-      const index = (typeof item === "object") ? item.id : null || destinationArray.length+i
-      byId[index] = Object.assign(byId[index] || {}, item);
-    });
-    return Object.values(byId);
-  }
-}
+const joinOn = joinKey => (values, join) => {
+  const individualsById = {};
+  flat(values).forEach((ind, i) => {
+    const idValue = ind[joinKey] || `__graphqlGatewayID-${i}`;
+    const existing = individualsById[idValue];
+    let result = existing || [];
+    result.push(ind);
+    individualsById[idValue] = result;
+  });
+  return Object.values(individualsById).map(
+    individuals =>
+      individuals.length > 1 ? join(...individuals) : individuals[0]
+  );
+};
 
-module.exports = function graphqlGateway(metaSchema, query, mergeOptions) {
-  return merge.all(
+const last = values => values[values.length - 1];
+
+const defaultMergeResolvers = {
+  Array: joinOn("id"),
+  String: last,
+  Number: last,
+  typeFromObj: obj => {
+    if (Array.isArray(obj)) return "Array";
+    if (typeof obj === "string") return "String";
+    if (typeof obj === "number") return "Number";
+    if (typeof obj === "object") return obj.__typename;
+  }
+};
+
+module.exports = function graphqlGateway(metaSchema, query, mergeResolvers) {
+  const merge = mergeResolver({ ...defaultMergeResolvers, ...mergeResolvers });
+  return merge(
     metaSchema.map(function(schemaInfo) {
-      return schemaInfo.resolve(graphqlMask(schemaInfo.schema, query));
-    }),
-    mergeOptions || defaultMergeOptions
+      return schemaInfo.resolve(graphqlMask({schema: schemaInfo.schema, query}).maskedQuery);
+    })
   );
 };
